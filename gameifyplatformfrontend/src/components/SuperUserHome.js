fbaef301690ac162bc93e764daec9a391c6d0a55
Luke Sanger <lukesngr@gmail.com>
2024-03-16 21:23:07 +1100

    Looking good time to add the code

--- gameifyplatformfrontend/src/components/SuperUserHome.js
+++ gameifyplatformfrontend/src/components/SuperUserHome.js
@@ -1,22 +1,34 @@
 import { useState } from "react"
 import NewTask from "./NewTask"
 import Navbar from "./Navbar";
+import '../styles/userhome.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export default function SuperUserHome() {
-    const [tasks, setTasks] = useState(['1', '2']);
+    const [tasks, setTasks] = useState([{taskName: 'fd'}, {taskName: 'fdfd'}]);
     const [taskName, setTaskName] = useState("");
     const [taskDescription, setTaskDescription] = useState("");
 
+    function deleteTask(index) {
+        setTasks(previousState => {
+            return previousState.filter((_, i) => i !== index);
+        });
+    }
+
     return (
-        <div>
+        <>
             <Navbar />
-            <div className="superUserHome">
+            <div className="tasksContainer">
                 <h1>Create List</h1>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addTaskModal">Add Task</button>
+                <button type="button" data-toggle="modal" data-target="#addTaskModal">Add Task</button>
                 {tasks.map((task, index) => {
-                    return <p>{task}</p>;
+                    return <div className="task" key={index}>
+                    <p className="createdTaskName">{task.taskName}</p>
+                    <button className="deleteCreatedTask" onClick={() => deleteTask(index)}><FontAwesomeIcon icon={faTrash} /></button>
+                    </div>;
                 })}
-                <button>Create</button>
+                <button>Send to Server</button>
             </div>
             <div class="modal fade" id="addTaskModal" tabindex="-1" role="dialog" aria-labelledby="addTaskModal" aria-hidden="true">
                 <div class="modal-dialog" role="document">
@@ -30,19 +42,18 @@ export default function SuperUserHome() {
                         <div class="modal-body">
                             <form className="newTask">
                                 <label for="taskName">Task Name</label>
-                                <input id="taskName" type="text" value={taskName} onChange={setTaskName}></input>
+                                <input id="taskName" type="text" value={taskName} onChange={e => setTaskName(e.target.value)}></input>
                                 <label for="taskDescription">Task Description</label>
-                                <input id="taskDescription" type="text"  value={taskDescription} onChange={setTaskDescription}></input>
+                                <input id="taskDescription" type="text"  value={taskDescription} onChange={e => setTaskDescription(e.target.value)}></input>
                             </form>
-                            <button onClick={() => setTasks([...tasks, {taskName, taskDescription}])}>Add</button>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" onClick={() => setTasks(previousState => ([...previousState, {taskName, taskDescription}]))}>Save</button>
+                            <button type="button" data-dismiss="modal">Close</button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
